67243b702300b0c4eec1dc2d41766f781a827fc9
Franco Bustos <leocarpiotombino@gmail.com>
2025-10-23 12:20:27 -0300

    bucles de control para determinar si un número es positivo o negativo.

---
+++ script.js
@@ -0,0 +1,53 @@
+console.log("inicio del script");
+
+console.log("que tal");
+
+let Nombre = "Positivo";
+console.log(Nombre);
+
+const edad = 0;
+console.log(edad);
+
+if (Nombre === "Positivo") {
+  console.log("El numero es positivo");
+} else {
+  console.log("El numero es negativo");
+}
+
+if (edad === 0) {
+  console.log("El numero es positivo");
+} else {
+  console.log("El numero es negativo");
+}
+
+switch (edad) {
+  case 0:
+    console.log("El numero es positivo");
+    break;
+  case -1:
+    console.log("El numero es negativo");
+    break;
+  default:
+    console.log("El numero no es reconocido");
+    break;
+}
+
+for (let positivo = 0; positivo < 0; positivo++) {
+  console.log("El valor de positivo es: " + positivo);
+}
+
+for (let negativo = 0; negativo > 0; negativo--) {
+  console.log("El valor de negativo es: " + negativo);
+}
+
+let positivo = 0;
+while (positivo >= 0) {
+  console.log("El valor del numero es: " + positivo);
+  positivo++;
+}
+
+let negativo = 0;
+while (negativo <= 0) {
+  console.log("El valor del numero es: " + negativo);
+  negativo--;
+}
